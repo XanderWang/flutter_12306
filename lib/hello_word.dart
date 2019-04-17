590b1431e8a5f7a32358cb6d5e985ee69af5c89e
XanderWang <420640763@qq.com>
2019-04-17 18:53:28 +0800

    添加 demo

---
+++ lib/hello_word.dart
@@ -0,0 +1,45 @@
+import 'package:dio/dio.dart';
+
+/**
+ * 配合安装 dart sdk , 方便调试
+ * dart sdk 可以在 flutter\bin\cache\dart-sdk\bin 下面找到
+ */
+
+void main() {
+  print("hello flutter");
+  print("你好啊");
+  print(new TestA(1, 2));
+  print(TestA.empty());
+  print(TestA.forX(33));
+  print(TestA.forY());
+
+  getNet_3();
+}
+
+void getNet_3() async {
+  var url = 'https://wanandroid.com/wxarticle/chapters/json';
+  Dio dio = new Dio();
+  var response = await dio.get(url);
+  print("getNet_3 $response.data.toString()");
+}
+
+class TestA {
+  num x = -1, y = -1;
+  TestA(this.x, this.y);
+
+  TestA.empty() {
+    x = 0;
+    y = 0;
+  }
+
+  TestA.forX(this.x);
+
+  TestA.forY() {
+    y = 100;
+  }
+
+  @override
+  String toString() {
+    return "x=$x , y=$y";
+  }
+}
